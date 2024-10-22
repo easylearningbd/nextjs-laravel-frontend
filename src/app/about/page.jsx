aa0037fd9033482fe91d2dad077ad76289ea09e1
easylearningbd <elebdsoft@gmail.com>
2024-10-23 05:36:24 +0600

    34 About US Page Design Part 4

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,7 +4,10 @@ import TopNav from '@/components/Header/TopNav/TopNav';
 import Partner from '@/components/Partner/Partner';
 import AboutSection from '@/components/Section/AboutSection';
 import Breadcrumb from '@/components/Section/Breadcrumb';
+import Counter from '@/components/Section/Counter';
+import Service from '@/components/Service/Service';
 import React from 'react';
+import serviceData from '@/data/service.json'
 
 const AboutPage = () => {
     return (
@@ -17,6 +20,8 @@ const AboutPage = () => {
      <main className="content">
         <Breadcrumb link="About Us" img="/images/header.webp" title='About Us' desc="The jobs report soundly beat expectations, with job gains broadly spread across the economy and about 60% higher" />
         <AboutSection/>
+        <Counter classname="lg:pb-[50px] sm:pb-16 pb-10" />
+        <Service data={serviceData} />
      </main>
 
      <Partner className='lg:mt-[100px] sm:mt-16 mt-10' /> 
