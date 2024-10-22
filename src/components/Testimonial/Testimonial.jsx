d7c173958b1381f6c2982035af3e88f556c8a75e
easylearningbd <elebdsoft@gmail.com>
2024-10-23 00:30:03 +0600

    24 Testimonial Component Design Part 2

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -28,8 +28,22 @@ const Testimonial = () => {
         }}
         >
 
-     <SwiperSlide>
+     <SwiperSlide className='lg:pb-24 pb-20'>
+        <div className='text-2xl font-medium text-center'>
+            {String.raw`"`}Get personalized financial advice to help reach your financial goals.Get personalized financial advice to help reach your financial goals. {String.raw`"`}
+        </div>
+        <div className='text-button text-center mt-5'>
+            Kazi Ariyan // CEO
+        </div> 
+     </SwiperSlide>
 
+     <SwiperSlide className='lg:pb-24 pb-20'>
+        <div className='text-2xl font-medium text-center'>
+            {String.raw`"`}Get personalized financial advice to help reach your financial goals.Get personalized financial advice to help reach your financial goals. {String.raw`"`}
+        </div>
+        <div className='text-button text-center mt-5'>
+            Khan // Manager
+        </div> 
      </SwiperSlide>
 
 
