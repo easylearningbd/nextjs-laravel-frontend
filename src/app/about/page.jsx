f3ab2da38a38b098217e486f8d1116635ba65f5a
easylearningbd <elebdsoft@gmail.com>
2024-10-23 04:52:31 +0600

    33 About US Page Design Part 3

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,6 +2,7 @@ import Footer from '@/components/Footer/Footer';
 import Menu from '@/components/Header/Menu/Menu';
 import TopNav from '@/components/Header/TopNav/TopNav';
 import Partner from '@/components/Partner/Partner';
+import AboutSection from '@/components/Section/AboutSection';
 import Breadcrumb from '@/components/Section/Breadcrumb';
 import React from 'react';
 
@@ -15,6 +16,7 @@ const AboutPage = () => {
 
      <main className="content">
         <Breadcrumb link="About Us" img="/images/header.webp" title='About Us' desc="The jobs report soundly beat expectations, with job gains broadly spread across the economy and about 60% higher" />
+        <AboutSection/>
      </main>
 
      <Partner className='lg:mt-[100px] sm:mt-16 mt-10' /> 
